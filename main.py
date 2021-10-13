a3bb5fde51f00e58dc3f008ffb6f8b91b640b1ed
Ricardo Fernández <ricardo.a.fernandez.a@gmail.com>
2021-10-12 20:45:02 -0500

    Versión 1 del Proyecto Final

---
+++ main.py
@@ -0,0 +1,140 @@
+from random import *
+from preguntas import algebra, geometria, estadistca
+import matplotlib.pyplot as plt
+import numpy as np
+
+global areas
+global puntos
+
+
+areas = {'Álgebra':0, 'Geometría':0, 'Estadística':0}
+
+def menu():
+
+    print("Práctica Examen Pisa")
+    print("1. Practicar Álgebra")
+    print("2. Practicar Geometría")
+    print("3. Practicar Estadística")
+    print("4. Ver resultados")
+    print("5. Salir")
+
+    opcion = input('Ingrese una opción: ')
+
+    if opcion == '1':
+        examenPrincipal(algebra)
+    
+    elif opcion == '2':
+        examenPrincipal(geometria)
+
+    elif opcion == '3':
+        examenPrincipal(estadistca)
+
+    elif opcion == '4':
+        graficar()
+
+    elif opcion == '5':
+
+        areasLlaves = areas.keys()
+        areasValores = [str(areas[i]) for i in areasLlaves]
+        f = open('register.txt', 'w')
+        f.write(' '.join(areasValores))
+        f.close()
+
+    else:
+        print('Opción inválida')
+        menu()
+
+    
+
+def examenPrincipal(area):
+
+    if area == estadistca:
+        areas['Estadística'] = 0
+
+    elif area == algebra:
+        areas['Álgebra'] = 0
+
+    else:
+        areas['Geometría'] = 0
+
+    puntos = []
+    selecPregun = sample(area, 4)
+
+    for i in range(4):
+        respsRan = sample(selecPregun[i][1:],4)
+
+        print(f'{i+1}) {selecPregun[i][0]}')
+        
+        for j in range(4):
+            alph = 'abcd'
+            print(f'{alph[j]}) {respsRan[j]}')
+
+        
+
+        while True:
+
+            resp = input('Su respuesta es: ').lower()
+            
+            try:
+
+                if resp == '':
+                    pass
+                
+                
+                elif respsRan[alph.index(resp)] == selecPregun[i][1]:
+                    puntos.append(f'Pregunta {i+1}: Correcta')
+                    
+                    if area == estadistca:
+                        areas['Estadística'] = areas['Estadística'] + 1
+
+                    elif area == algebra:
+                        areas['Álgebra'] = areas['Álgebra'] + 1
+
+                    else:
+                        areas['Geometría'] = areas['Geometría'] + 1
+
+
+                else:
+                    puntos.append(f'Pregunta {i+1}: Incorrecta')
+
+            except:
+                pass
+
+            if resp in 'abcd' and resp != '':
+                print()
+                break
+            else:
+                print('Respuesta no válida')
+
+    print('\n'.join(puntos))
+
+    menu()
+
+def graficar():
+    
+    f = open('register.txt', 'r')
+
+    print(areas)
+
+    valoresAnteriores = f.read().split(' ')
+    valoresAnteriores = [int(i) for i in valoresAnteriores]
+
+    f.close()
+
+    areasLlaves = areas.keys()
+    areasValores = [areas[i] for i in areasLlaves]
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle('Calificación por fecha')
+    fig.set_size_inches(13, 5)
+    ax1.bar(areasLlaves,valoresAnteriores)
+    ax2.bar(areasLlaves,areasValores)
+    ax1.set(xlabel = 'Área', ylabel = 'Puntaje (de 4)')
+    ax2.set(xlabel = 'Área', ylabel = 'Puntaje (de 4)')
+    ax1.set_title('Intento Anterior')
+    ax2.set_title('Intento Actual')
+    fig.show()
+
+    menu()
+
+menu()
---
+++ preguntas.py
@@ -0,0 +1,105 @@
+algebra = [
+    
+    ['Para los hombres, existe una fórmula que relaciona la longitud de la pisada y el número de pasos. Es la siguiente: n/P = 140, dónde la n representa el número de pasos por minuto y la P la longitud del paso en metros.\nSi Enrique da 70 pasos por minuto, ¿cuál es la longitud de su pisada?', 
+        '0.5 m', '5 cm', '1 m', '1.5 m'],
+
+    ['Para los hombres, existe una fórmula que relaciona la longitud de la pisada y el número de pasos. Es la siguiente: n/P = 140, dónde la n representa el número de pasos por minuto y la P la longitud del paso en metros.\nBernardo sabe que sus pasos son de 0.8 m y que la fórmula se ajusta a él. ¿Cuál es su velocidad en metros por minuto?',
+        '89.6 m/min', '112 m/min', '100.2 m/min', '74.6 m/min'],
+
+    ['Para los hombres, existe una fórmula que relaciona la longitud de la pisada y el número de pasos. Es la siguiente: n/P = 140, dónde la n representa el número de pasos por minuto y la P la longitud del paso en metros.\nBernardo sabe que sus pasos son de 0.8 m y que la fórmula se ajusta a él. ¿Cuál es su velocidad en kilómetros por hora?',
+        '5.38 km/h', '10 km/h', '7.29 km/h', '4.38 km/h'],
+
+    ['En los dados existe una regla que dice que la suma de dos caras opuestas siempre es 7.\nDadas las siguientes caras, elige la respuesta con sus caras opuestas: 6, 2, 3, 5, 1, 2',
+        '1, 5, 4, 2, 6, 5', '1, 5, 4, 2, 6, 3', '5, 3, 4, 2, 1, 3', '2, 6, 2, 4, 1, 3'],
+
+    ['Si cuando en Berlín es la 1:00 am en Sydney son las 10:00 am, ¿qué hora es en Berlín cuando en Sydney son las 7:00 pm?', 
+        '10:00 am', '10:00 pm', '9:00 am', '3:00 pm'],
+
+    ['Mark vive en Sydney y Hans en Berlín. Cuando en Berlín es la 1:00 am en Sydney son las 10:00 am. Ambos NO pueden hablar desde las 9:00 am hasta las 4:30 pm y desde las 11:00 pm, hasta las 7:00 am. ¿A qué hora podrán hablar Mark y Hans?',
+        'Sydney: 7:30 am, Berlín: 10:30 pm', 'Sydney: 6:00 am, Berlín: 9:00 pm', 'Sydney: 6:30 am, Berlín: 9:30 pm', 'Sydney: 4:00 pm, Berlín: 7:00 pm'],
+
+    ['El cambio entre el Dólar de Singapur y el rand Sudafricano es de: 1 SGD = 4.2 ZAR.\nSi Mei-Ling cambió 3000 dólares de Singapur, ¿cuánto recibió en rands Sudafricanos?', 
+        '12600 ZAR', '12600 SDG', '1200 ZAR', '714.28 ZAR'],
+
+    ['El cambio entre el Dólar de Singapur y el rand Sudafricano es de: 1 SGD = 4.2 ZAR.\nSi Mei-Ling cambió 21000 rands Sudafricanos, ¿cuánto recibió en dólares de Singapur?', 
+        '5000 SDG', '5000 ZAR', '88200 SDG', '21000 SDG'],
+        
+    ['Mei-Ling tiene su dinero en Dólares de Singapur. Si el cambio con el rand Sudafricano pasa de 1 SDG = 4 ZAR a 1 SDG = 2 ZAR, ¿por cuánto aumentaría o disminuiría su dinero en Rands Sudafricanos?', 
+        '.5 veces', '2 veces', 'Sería lo mismo', '.2 veces'],
+
+    ['Tomás, Ricardo, Luis y David fueron a un lugar para jugar ping-pong. Ahí, ellos rentaron dos mesas.\nSi todos quieren jugar contra los demás al menos una vez, ¿cuántas rondas deben jugar entre ellos para lograrlo?', 
+        '3', '2', '1', '4']
+]
+
+
+geometria = [
+
+    ['La Tierra tiene aproximadamente un diámetro de 12700 km. La estación espacil Mir orbitaba la tierra a una altura de 400 km. A lo largo de su vida, la Mir dió aproximadamente 86500 vueltas a la tierra. ¿Cuántos kilómetros recorrió en total?', 
+        '3668 millones de Km', '4125 millones de km', '2723 millones de km', '6123 millones de km'],
+    
+    ['Imagina una escalera con 14 escalones, los cuales miden lo mismo a lo alto y a lo ancho. Toda la escalera mide 252 cm de alto y 400 cm de profundo. ¿Cuál es la altura de cada escalón?', 
+        '18 cm', '15 cm', '28.6 cm', '33 cm'],
+
+    ['Imagina una escalera con 14 escalones, los cuales miden lo mismo a lo alto y a lo ancho. Toda la escalera mide 252 cm de alto y 196 cm de profundo. ¿A qué profundidad estará el 3er escalón?', 
+        '42 cm', '14 cm', '28 cm', '66 cm'],
+
+    ['Imagina que hay un edificio con 20 pisos, contando la planta baja y que cada piso mide aproximadamente 2.5 metros de alto. ¿Cuál es la altura del edificio?', 
+        '50 m', '100 m', '25 m', '75 m'],
+
+    ['Nicolás quiere pavimentar su nuevo patio rectangular. Éste mide 5.25 m de largo por 3 m de ancho. Si necesita 81 ladrillos por metro cuadrado, ¿Cuántos ladrillos debe comprar Nicolás?', 
+        '1275', '1300', '16', '1255'], 
+
+    ['Una pizzería sirve 4 pizzas con las siguientes características (diámetro, precio): 30cm, 30 euros; 40 cm, 40 euros; 50 cm, 65 euros y 25 cm, 20 euros' ,
+        '2', '1', '3', '4'],
+
+    ['Imagina un tejado inclinado. La distancia entre el centro del tejado y su extremo es de 2.5m y el punto más alto del tejado es de 1m. Además, el tejado mide 6m a lo largo. Calcula el área total del tejado.',
+        '32.4 m^2', '16.2 m^2', '20.2 m^2', '12.6m^2'],
+
+    ['Imagina una puerta giratoria. Dentro del círculo que siguen las puertas, hay tres de estas. Cálcula el ángulo que forman entre ellas',
+        '120°', '60°', '85°', '90°'],
+
+    ['Imagina una puerta giratoria. Dentro del círculo que siguen las puertas, hay tres de estas. Si las puertas completan 4 vueltas en un minuto y entre cada puerta hay espacio para dos personas, ¿cuál es el máximo de personas que pueden pasar por la puerta en 30 minutos?',
+        '720', '240', '180', '60'],
+
+    ['Imagina una noria. El punto más alto de ésta está a los 150 metros sobre el mar. Por otro lado, la base de la Noria está en una plataforma a 10 metros sobre el nivel del mar. Encuentra el radio de la noria.',
+        '70 m', '170 m', '140 m', '150 m']
+
+    
+]
+
+estadistca = [
+
+    ['Las emisiones de CO2 de Estados Unidos fueron de 6049 toneladas en 1990 y 6727 en 1998. ¿Qué porcentaje aumentaron las emisiones en ese periodo?', 
+        '11%', '10%', '15%', '8.5%'],
+
+    ['Las emisiones de CO2 de Estados Unidos fueron de 6049 toneladas en 1990 y aumentaron un 15%. ¿Cuáles fueron las emisiones de Estados Unidos en 1998?', 
+        '6956 Toneladas', '7123 Toneladas', '6478 Toneladas', '6649 Toneladas'],
+
+    ['En una bolsa hay 6 caramelos rojos, 5 Naranjas, 3 Amarillos, 3 Verdes, 2 Azules, 4 Rosas, 2 Violetas y 5 Marrones. ¿Cuál es la probabilidad de que saque un caramelo rojo?', 
+        '20%', '10%', '25%', '50%'],
+    
+    ['En una bolsa hay 6 caramelos rojos, 5 Naranjas, 3 Amarillos, 3 Verdes, 2 Azules, 4 Rosas, 2 Violetas y 5 Marrones. ¿Cuál es la probabilidad de que saque un caramelo azul?', 
+        '6%', '10%', '25%', '50%'],
+
+    ['En una baraja hay 52 cartas. ¿Cuál es la probabilidad de sacar un as si en la baraja hay 4 de estos?', 
+        '1/13', '5/53', '4/53', '3/52'],
+    
+    ['Irene tiene un promedio de 60 en sus últimos 4 exámenes. En el último sacó 80. ¿Cuál será su nuevo promedio?', 
+        '64', '70', '62', '65'],
+
+    ['Irene tiene un promedio de 65 en sus últimos 4 exámenes. ¿Cuánto necesita sacar para tener un promedio de 70?', 
+        '90', '100', '85', '75'],
+
+    ['En la feria hay un juego que consiste en lo siguiente: Una ruleta con los números del 1 al 6 y una bolsa con 10 canicas, de las cuales 3 son negras. Cada vez que caiga un número par en la ruleta, se puede sacar una canica. Se gana sacando una canica negra. ¿Cuál es la probabilidad de ganar el juego?', 
+        '3/20', '1/2', '3/10', '6/10'],
+    
+    ['Un geólogo dijo: "En los próximos 20 años, la posibilidad de que ocurra un terremoto es dos de tres". ¿Qué enunciado representa mejor lo que dijo?', 
+        '(2/3)*20 = 13.3, por lo que es probable que haya un terremoto dentro de 13 o 14 años', 
+        '2/3 es más que 1/2, por lo que es seguro que habrá un terremoto',
+        'La probabilidad de que haya un terremoto es mayor a que no la haya',
+        'No se puede determinar que es lo que sucederá.'],
+
+    ['En una pizzería, aparte de queso y tomate, le puedes poner hasta dos ingredientes extra a tu pizza. En la pizzería cuentan con 4 ingredientes. ¿Cuántas combinaciones existen combinando dos ingredientes diferentes?', 
+        '6', '10', '24', '4']
+
+]
